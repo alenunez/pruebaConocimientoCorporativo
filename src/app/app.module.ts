98f43de0dfe20ecbf2f9151db457b03ca25afa01
alenunez097 <alenunez097@gmail.com>
2022-03-30 01:47:32 -0500

    Actualizaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { BarraSuperiorComponent } from './components/barra-superior/barra-superior.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    BarraSuperiorComponent,
   ],
   imports: [
     BrowserModule,
